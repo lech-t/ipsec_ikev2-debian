a59a13e5b805d9c2a88f5cf1573aa2e53fa42442
lech-t <66976100+lech-t@users.noreply.github.com>
2020-06-16 00:24:48 +0200

    Update ipsec-setup.sh

--- ipsec-setup.sh
+++ ipsec-setup.sh
@@ -3,23 +3,15 @@
 #@(#) IPSec Setup
 # by Lech Taczkowski
 #------------------------------------------------------------------------------
-#< Description:   This script is useful for setting up IPSec server with
-#                 Strongswan + IKEv2 + key authentication (Roadwarrior).
-#		  It allows users to get secure access to another network over
-#		  an unsecure network (Internet). It also routes Internet
-#		  traffic through the tunnel.
-#
-#		  It has been written for my own personal use for the following
-#		  setup, where the VPN server is behind a router and is NATted.
-#		  The ports are being redirected. 
-#		  There are so many possible scenarios of setting up StrongSwan
-#		  with IKEv2. This script is only for one of them.
-#
-#		  I hope it's going to be useful.
-#		  Stay safe!
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This script can be useful for setting up IPSec server with Strongswan + IKEv2 + key authentication (Roadwarrior).
+# It allows users to get secure access to another network over an unsecure network (Internet).
 
-clear
+# Script has been written for my own personal use for the following setup, where the VPN server is behind a router and is NATted.
+# The ports should be redirected on the router.
+# There are so many possible scenarios of setting up StrongSwan with IKEv2. This script focuses on one of them.
+
+# I hope it's going to be useful.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Debug mode
 # set -x
